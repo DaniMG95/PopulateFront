6d4da9b6a7298c6fb7dd789fba930e7d4f4ef5fa
DaniMG95 <dani_16_5_95@hotmail.com>
2021-09-30 18:47:19 +0200

    Create Modal and get data of API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {MatDialog} from "@angular/material/dialog";
+import {DialogComponent} from  'src/app/dialog/dialog.component'
+
+
+export interface DialogData {
+  review: string;
+  rating: number;
+}
+
+const URL: string = "http://localhost:5000"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'populateFront';
+
+  item: any;
+  data!: DialogData
+
+  constructor(private http:HttpClient, public dialog: MatDialog) {
+  }
+
+  ngOnInit(): void {
+    this.get_data()
+  }
+
+  get_data(){
+    this.http.get(`${URL}/item/1`).subscribe(data =>{
+      this.item = data
+    })
+  }
+
+  createReview(): void {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '250px',
+      data: {rating: 0, review: ""}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.data = result;
+      this.http.post(`${URL}/review/1`, this.data).subscribe(data =>{
+        console.log(data)
+        this.http.put(`${URL}/item/1`,{}).subscribe(data =>{
+          console.log(data)
+          this.get_data()
+        })
+      })
+    });
+  }
+
+
+
+
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgRatingBarModule } from 'ng-rating-bar';
+import { DialogComponent } from './dialog/dialog.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {FormsModule} from "@angular/forms";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent
   ],
   imports: [
-    BrowserModule
+    MatInputModule,
+    MatDialogModule,
+    BrowserModule,
+    HttpClientModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    NgRatingBarModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
